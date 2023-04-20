44bc9fc7244c5a68354caa4175914962f081264c
shervin <shervingouhari@gmail.com>
2023-04-20 11:26:04 +0330

    password_change functionallity added.

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeDoneView
 from . import views
 
 app_name = 'account'
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('password_change/', views.PasswordChange.as_view(), name='password_change'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import PasswordChangeView
+from django.urls import reverse_lazy
 
 
 @login_required
 def profile(request):
     return render(request, 'account/profile.html')
+
+
+class PasswordChange(PasswordChangeView):
+    success_url = reverse_lazy('account:password_change_done')
