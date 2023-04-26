0c50e4602236b58bb0c964396c94992ecb9fc022
shervin <shervingouhari@gmail.com>
2023-04-27 00:29:20 +0330

    certain fixes.

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
+from django.forms.models import model_to_dict
 
 from user.models import User
 from user.forms import *
@@ -15,9 +16,9 @@ def registration(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid() and form.clean_password2():
-            user = form.save()
-            phone_number = user.phone_number
-            return redirect(reverse('verification_code', kwargs={'phone_number': phone_number}))
+            unsaved_user = form.save(commit=False)
+            request.session['unsaved_user'] = model_to_dict(unsaved_user, exclude='avatar')
+            return redirect(reverse('verification_code'))
         else:
             messages.error(request, form.errors)
         data = {
@@ -28,16 +29,17 @@ def registration(request):
     return render(request, 'account/registration.html', {'data': data})
 
 
-def verification_code(request, phone_number):
-    ver_code = send_sms(request, phone_number)
+def verification_code(request):
+    unsaved_user = request.session['unsaved_user']
+    ver_code = send_sms(request, unsaved_user['phone_number'])
     form = VerificationForm()
     if request.method == 'POST':
-        user = User.objects.get(phone_number=phone_number)
         form = VerificationForm(request.POST)
         if form.is_valid():
             if ver_code == form.cleaned_data['verification_code']:
-                user.is_active = True
-                user.save()
+                unsaved_user['is_active'] = True
+                user = User.objects.create(**unsaved_user)
+                del request.session['unsaved_user']
                 logout(request)
                 login(request, user)
                 return redirect(reverse('edit_profile'))
