070150a54e91d4d97c91c3ceccef9d24e9b4cc91
shervin <shervingouhari@gmail.com>
2023-06-06 20:50:19 +0330

    cm

--- post/templatetags/post_tags.py
+++ post/templatetags/post_tags.py
@@ -1,10 +1,14 @@
 from django import template
 from django.utils.timesince import timesince
 
+from datetime import datetime
+
 register = template.Library()
 
 
 @register.filter
 def customtimesince(value):
+    if type(value) == str:
+        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
     delta = timesince(value, depth=1)
     return delta
