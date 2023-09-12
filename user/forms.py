e5f360e2d057fe80724bd6c8b9cb278d2dde1097
shervin <shervingouhari@gmail.com>
2023-09-12 21:47:46 +0330

    date_of_birth fixed.

--- user/forms.py
+++ user/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.core.exceptions import ValidationError
+from datetime import datetime
 from .models import User
-from .utils import YEARS
+
 
 
 class UserCreationForm(forms.ModelForm):
@@ -34,7 +35,9 @@ class UserChangeForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['avatar', 'phone_number', 'username', 'email', 'first_name', 'last_name', 'date_of_birth', 'biography']
+        widgets = {
+            'date_of_birth': forms.DateInput(attrs={'type': 'date', 'max': datetime.now().date(), 'min': datetime(1920, 1, 1).date()})
+            }
 
     password1 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={"placeholder": "leave empty, if you don't wish to change"}), required=False)
     password2 = forms.CharField(label="Password confirmation", widget=forms.PasswordInput(attrs={"placeholder": "leave empty, if you don't wish to change"}), required=False)
-    date_of_birth = forms.DateField(widget=forms.SelectDateWidget(years=YEARS()), required=False)
