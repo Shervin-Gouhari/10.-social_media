e41341c2603236ef4783f94207056c21ef700d5d
shervin <shervingouhari@gmail.com>
2023-09-28 15:32:11 +0330

    .

--- action/models.py
+++ action/models.py
@@ -11,8 +11,8 @@ class Action(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="actions", on_delete=models.CASCADE)
     act = models.CharField(max_length=124)
     created = models.DateTimeField(auto_now_add=True)
-    target_ct = models.ForeignKey(ContentType, null=True, blank=True, related_name="actions", on_delete=models.CASCADE)
-    target_id = models.PositiveBigIntegerField(null=True, blank=True)
+    target_ct = models.ForeignKey(ContentType, related_name="actions", on_delete=models.CASCADE)
+    target_id = models.PositiveBigIntegerField()
     target = GenericForeignKey("target_ct", "target_id")
     
     def __str__(self):
