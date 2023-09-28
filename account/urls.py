e9169a6a1b91a2061b6baea2aaed0d55023b7b7d
shervin <shervingouhari@gmail.com>
2023-09-28 23:44:27 +0330

    .

--- account/urls.py
+++ account/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', views.custom_login, name='login'),
+    path('login/', views.login, name='login'),
     path('logout/', django_views.LogoutView.as_view(), name='logout'),
-    path('password_reset/', views.CustomPasswordReset.as_view(), name='password_reset'),
+    path('password_reset/', views.PasswordReset.as_view(), name='password_reset'),
     path('password_reset/done/', django_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', django_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', django_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
