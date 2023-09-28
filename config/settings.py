e54c43726a98b46d4d96d5c417d1bea25a8539eb
shervin <shervingouhari@gmail.com>
2023-09-29 00:08:39 +0330

    .

--- config/settings.py
+++ config/settings.py
@@ -117,7 +117,7 @@ AUTH_USER_MODEL = 'user.User'
 
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' if IN_DEVELOPMENT else 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' if IN_DEVELOPMENT else 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
