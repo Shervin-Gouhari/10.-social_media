b5ea3120fb769f9577570c64ca4975258586d6ec
shervin <shervingouhari@gmail.com>
2023-10-02 21:05:17 +0330

    .

--- static/js/profile/create_post.js
+++ static/js/profile/create_post.js
@@ -1,16 +1,20 @@
-let postPopUpButton = document.getElementById("post-pop-up-button");
-postPopUpButton.addEventListener("click", (e) => {
-    axios
-        .get("/post/create/")
-        .then((res) => {
-            Swal.fire({
-                width: 700,
-                title: "Create Your Post",
-                html: res.data.response,
-                showConfirmButton: false,
+let postPopUpButton1 = document.getElementById("post-pop-up-button1");
+let postPopUpButton2 = document.getElementById("post-pop-up-button2");
+[postPopUpButton1, postPopUpButton2].forEach((event) => {
+    event.addEventListener("click", (e) => {
+        e.preventDefault();
+        axios
+            .get("/post/create/")
+            .then((res) => {
+                Swal.fire({
+                    width: 700,
+                    title: "Create Your Post",
+                    html: res.data.response,
+                    showConfirmButton: false,
+                });
+            })
+            .catch((err) => {
+                console.log(err);
             });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    });
 });
